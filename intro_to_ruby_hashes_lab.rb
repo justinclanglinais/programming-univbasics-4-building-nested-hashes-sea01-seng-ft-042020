496815ae6ca8ed259596e564b7531223bc329ff8
Justin Langlinais <justinclanglinais@gmail.com>
2020-04-02 21:44:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,12 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 new_hash = {
-    :railroads => { :pieces => 4 }
+    :railroads => 
+      {:pieces => 4 },
+      {:rent_in_dollars =>
+        {
+          
+        }
   }
 end
 
